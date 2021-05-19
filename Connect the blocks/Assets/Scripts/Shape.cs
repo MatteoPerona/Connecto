d371b1cd017ae1ca8ec65d1e005917a0e0fd7a8b
MatteoPerona <63029740+MatteoPerona@users.noreply.github.com>
2021-05-19 11:11:56 -0700

    Unity Project
    here is the blank unity project

---
+++ Connect the blocks/Assets/Scripts/Shape.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : MonoBehaviour
+{
+    public int[,] array = new int[8,8];
+
+    public void GenerateShape()
+    {
+
+    }
+}
